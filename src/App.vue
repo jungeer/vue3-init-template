cdfdf2d40f9425102c597b9936d7749800256e9e
曾佳俊 <952432141@qq.com>
2023-12-27 15:17:25 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style>
+html,
+body,
+#app {
+  height: 100%;
+  width: 100%;
+}
+
+#app {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <h1 class="animate__animated animate__zoomInDown text-72px color-#e69138">
+    你好！
+  </h1>
+</template>
